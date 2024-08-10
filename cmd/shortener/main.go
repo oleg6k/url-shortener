1cb7bdadcaa0a74b089fcfba2bb07815093d4d9e
oleg6k <oleg6k@gmail.com>
2024-08-11 02:06:14 +0400

    1 iter

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,22 @@
 package main
 
-func main() {}
+import (
+	"github.com/oleg6k/url-shortener/internal/app"
+	"net/http"
+)
+
+func main() {
+	service := app.NewService()
+	controller := app.NewController("http://localhost:8080", service)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodPost {
+			controller.PostShorting(w, r)
+		} else if r.Method == http.MethodGet {
+			controller.GetRedirectToOriginal(w, r)
+		} else {
+			http.Error(w, "Not Found", http.StatusNotFound)
+		}
+	})
+	http.ListenAndServe(":8080", mux)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/oleg6k/url-shortener
+
+go 1.22.4
