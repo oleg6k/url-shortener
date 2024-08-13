97ef581d3f77f570d0bf6ddc16626b1bbf4b0dda
oleg6k <oleg6k@gmail.com>
2024-08-13 19:51:55 +0400

    iter4

--- cmd/shortener/flags.go
+++ cmd/shortener/flags.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"net/url"
 )
 
 type Flags struct {
@@ -16,5 +17,22 @@ func ParseFlags() *Flags {
 	flag.StringVar(&flags.BaseURL, "b", ":8080", "base address and port to shortener results")
 	flag.Parse()
 
+	runAddr, err := url.Parse(flags.RunAddr)
+	if err != nil {
+		panic("incorrect URL")
+	}
+
+	if runAddr.Host == "" {
+		flags.RunAddr = "http://localhost" + flags.RunAddr
+	}
+
+	baseURL, err := url.Parse(flags.BaseURL)
+	if err != nil {
+		panic("incorrect URL")
+	}
+
+	if baseURL.Host == "" {
+		flags.BaseURL = "http://localhost" + flags.BaseURL
+	}
 	return flags
 }
